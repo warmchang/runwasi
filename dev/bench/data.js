88c36c6f8d4c9c332c372429eee078b526988b5b
github-action-benchmark <github@users.noreply.github.com>
2025-03-01 01:27:53 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for 3240088e247f61952fa6b961ff61aa9799ae92be

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740707039349,
+  "lastUpdate": 1740792473010,
   "repoUrl": "https://github.com/warmchang/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -116,6 +116,52 @@ window.BENCHMARK_DATA = {
             "value": 84433828,
             "range": "± 847435",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jorge Prendes",
+            "username": "jprendes",
+            "email": "jorge.prendes@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "3240088e247f61952fa6b961ff61aa9799ae92be",
+          "message": "Merge pull request #876 from z63d/feat/refactor-cgroup-driver\n\nchore: refactor for handling of cgroup driver",
+          "timestamp": "2025-02-26T15:35:37Z",
+          "url": "https://github.com/warmchang/runwasi/commit/3240088e247f61952fa6b961ff61aa9799ae92be"
+        },
+        "date": 1740792471901,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15144,
+            "unit": "kB",
+            "extra": "shim: 12416 kB\nzygote: 2728 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 65860,
+            "unit": "kB",
+            "extra": "shim: 53504 kB\nzygote: 12356 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18176,
+            "unit": "kB",
+            "extra": "shim: 14848 kB\nzygote: 3328 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 18884,
+            "unit": "kB",
+            "extra": "shim: 15872 kB\nzygote: 3012 kB"
           }
         ]
       }
