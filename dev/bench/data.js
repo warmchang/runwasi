5d37bfa452ed3620c84c3f5353956ecb885032af
github-action-benchmark <github@users.noreply.github.com>
2026-03-26 02:42:47 +0000

    add HTTP Throughput (customBiggerIsBetter) benchmark result for d8a20ca148f0680874b6b87542e3cb07476d322f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774408481897,
+  "lastUpdate": 1774492967234,
   "repoUrl": "https://github.com/warmchang/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -55868,6 +55868,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "HTTP RPS",
             "value": 16710.0304,
+            "unit": "req/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d8a20ca148f0680874b6b87542e3cb07476d322f",
+          "message": "chore(deps): bump actions/upload-artifact from 4 to 7 (#1076)\n\nBumps [actions/upload-artifact](https://github.com/actions/upload-artifact) from 4 to 7.\n- [Release notes](https://github.com/actions/upload-artifact/releases)\n- [Commits](https://github.com/actions/upload-artifact/compare/v4...v7)\n\n---\nupdated-dependencies:\n- dependency-name: actions/upload-artifact\n  dependency-version: '7'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-03-25T23:17:01Z",
+          "url": "https://github.com/warmchang/runwasi/commit/d8a20ca148f0680874b6b87542e3cb07476d322f"
+        },
+        "date": 1774492952918,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "HTTP RPS",
+            "value": 15594.0038,
             "unit": "req/s"
           }
         ]
