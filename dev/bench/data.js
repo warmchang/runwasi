5c8dcf5ba5e3febca6c8f6a528a13b143a42911f
github-action-benchmark <github@users.noreply.github.com>
2026-04-17 02:39:26 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for eac1a498ab9ad0cfec386e6f6b4ea321ee16984d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776311755997,
+  "lastUpdate": 1776393566708,
   "repoUrl": "https://github.com/warmchang/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -47862,6 +47862,52 @@ window.BENCHMARK_DATA = {
             "value": 275282267,
             "range": "± 7147147",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jorge Prendes",
+            "username": "jprendes",
+            "email": "jorge.prendes@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "eac1a498ab9ad0cfec386e6f6b4ea321ee16984d",
+          "message": "update registry for apk-anywhere (#1107)\n\nSigned-off-by: Jorge Prendes <jorge.prendes@gmail.com>",
+          "timestamp": "2026-04-15T16:08:55Z",
+          "url": "https://github.com/warmchang/runwasi/commit/eac1a498ab9ad0cfec386e6f6b4ea321ee16984d"
+        },
+        "date": 1776393553243,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15084,
+            "unit": "kB",
+            "extra": "shim: 12136 kB\nzygote: 2948 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 66192,
+            "unit": "kB",
+            "extra": "shim: 53564 kB\nzygote: 12628 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18588,
+            "unit": "kB",
+            "extra": "shim: 15064 kB\nzygote: 3524 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 20416,
+            "unit": "kB",
+            "extra": "shim: 16920 kB\nzygote: 3496 kB"
           }
         ]
       }
