681307cc7cd1fea2bf7e3c9bd2ca0d34ef4de429
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 03:29:35 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for eb9967aae75bfc8ddf810590810f6962a8dc7c37

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777518003728,
+  "lastUpdate": 1777606175399,
   "repoUrl": "https://github.com/warmchang/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -49486,6 +49486,52 @@ window.BENCHMARK_DATA = {
             "value": 302221487,
             "range": "± 6674306",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "eb9967aae75bfc8ddf810590810f6962a8dc7c37",
+          "message": "chore(deps): bump dorny/paths-filter from 3 to 4 (#1090)\n\nBumps [dorny/paths-filter](https://github.com/dorny/paths-filter) from 3 to 4.\n- [Release notes](https://github.com/dorny/paths-filter/releases)\n- [Changelog](https://github.com/dorny/paths-filter/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/dorny/paths-filter/compare/v3...v4)\n\n---\nupdated-dependencies:\n- dependency-name: dorny/paths-filter\n  dependency-version: '4'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-04-21T13:18:25Z",
+          "url": "https://github.com/warmchang/runwasi/commit/eb9967aae75bfc8ddf810590810f6962a8dc7c37"
+        },
+        "date": 1777606162208,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15648,
+            "unit": "kB",
+            "extra": "shim: 12560 kB\nzygote: 3088 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 66816,
+            "unit": "kB",
+            "extra": "shim: 54244 kB\nzygote: 12572 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18980,
+            "unit": "kB",
+            "extra": "shim: 15452 kB\nzygote: 3528 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 20208,
+            "unit": "kB",
+            "extra": "shim: 16812 kB\nzygote: 3396 kB"
           }
         ]
       }
