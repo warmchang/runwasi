0f84a6ab870302ec32d42c9401d186c472b71882
github-action-benchmark <github@users.noreply.github.com>
2026-05-25 03:54:54 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for be1ca93e8fb155a2c3beb68c396f1ffd2a87bad8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779594547808,
+  "lastUpdate": 1779681293927,
   "repoUrl": "https://github.com/warmchang/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -52270,6 +52270,52 @@ window.BENCHMARK_DATA = {
             "value": 71236637,
             "range": "± 5553463",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Mateo Lelong",
+            "username": "TheRayquaza",
+            "email": "77465848+TheRayquaza@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "be1ca93e8fb155a2c3beb68c396f1ffd2a87bad8",
+          "message": "feat: shimkit: implement info flag (#1120)\n\nSigned-off-by: Mateo Lelong <mateo.lelong@datadoghq.com>",
+          "timestamp": "2026-05-21T16:07:33Z",
+          "url": "https://github.com/warmchang/runwasi/commit/be1ca93e8fb155a2c3beb68c396f1ffd2a87bad8"
+        },
+        "date": 1779681274207,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15792,
+            "unit": "kB",
+            "extra": "shim: 12724 kB\nzygote: 3068 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 66656,
+            "unit": "kB",
+            "extra": "shim: 54016 kB\nzygote: 12640 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 19580,
+            "unit": "kB",
+            "extra": "shim: 15848 kB\nzygote: 3732 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 20620,
+            "unit": "kB",
+            "extra": "shim: 16996 kB\nzygote: 3624 kB"
           }
         ]
       }
