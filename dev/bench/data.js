b1e12e6fa8ce5895b6b06eda4e04ae5e77405d49
github-action-benchmark <github@users.noreply.github.com>
2026-09-04 03:14:09 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for f1401719a3e981956ced9cd685abb2a49190bd1c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788405423731,
+  "lastUpdate": 1788491649045,
   "repoUrl": "https://github.com/warmchang/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -64056,6 +64056,52 @@ window.BENCHMARK_DATA = {
             "value": 84356642,
             "range": "± 737721",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "James Sturtevant",
+            "username": "jsturtevant",
+            "email": "jsturtevant@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "f1401719a3e981956ced9cd685abb2a49190bd1c",
+          "message": "Merge pull request #1153 from containerd/dependabot/cargo/patch-e21a0282c3\n\nchore(deps): bump the patch group across 1 directory with 3 updates",
+          "timestamp": "2026-06-13T00:38:57Z",
+          "url": "https://github.com/warmchang/runwasi/commit/f1401719a3e981956ced9cd685abb2a49190bd1c"
+        },
+        "date": 1788491623816,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15364,
+            "unit": "kB",
+            "extra": "shim: 12336 kB\nzygote: 3028 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 74916,
+            "unit": "kB",
+            "extra": "shim: 62144 kB\nzygote: 12772 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 19956,
+            "unit": "kB",
+            "extra": "shim: 15972 kB\nzygote: 3984 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 20160,
+            "unit": "kB",
+            "extra": "shim: 16396 kB\nzygote: 3764 kB"
           }
         ]
       }
